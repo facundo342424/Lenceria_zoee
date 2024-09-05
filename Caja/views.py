3a677d4073b907323695cba92202e1cd2d02351d
facundo342424 <c.m728916@gmail.com>
2024-09-05 19:12:27 -0300

    U_U

--- Caja/views.py
+++ Caja/views.py
@@ -1,17 +1,13 @@
 # Caja/views.py
 
 # Caja/views.py
-
 from django.shortcuts import render
-from .models import Caja
 
 def Apertura_de_caja(request):
-    cajas = Caja.objects.filter(fecha_cierre_caja__isnull=True)
-    return render(request, 'Apertura_de_caja', {'cajas': cajas})
+    return render(request, 'Apertura_de_caja.html')
 
 def Cierre(request):
-    cajas = Caja.objects.filter(fecha_cierre_caja__isnull=False)
-    return render(request, 'Cierre.html', {'cajas': cajas})
+    return render(request, 'Cierre.html')
 
 
 
